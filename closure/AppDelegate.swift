0445bf0fc7c8c6a2c13723599a91583a21c958b7
thierryH91200 <thierry.hentic@gmail.com>
2020-05-02 11:18:54 +0200

    Initial Commit

---
+++ closure/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  closure
+//
+//  Created by thierry hentic on 02/05/2020.
+//  Copyright © 2020 thierry hentic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ closureTests/closureTests.swift
@@ -0,0 +1,34 @@
+//
+//  closureTests.swift
+//  closureTests
+//
+//  Created by thierry hentic on 02/05/2020.
+//  Copyright © 2020 thierry hentic. All rights reserved.
+//
+
+import XCTest
+@testable import closure
+
+class closureTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
